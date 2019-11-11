9795f3ff6c403cb8921b401ec1d8e021ffed326e
unknown <you@example.com>
2019-11-11 22:03:51 +0800

    新增一个uers

---
+++ user/Delete.java
@@ -0,0 +1,20 @@
+package user;
+
+public class Delete implements Runnable{
+	private Storage st;
+	Delete(Storage st){
+		this.st=st;
+		
+	}
+	public void run(){
+		while(true){
+			st.get();
+			try{
+				Thread.sleep(1000);
+			} catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+
+}
+}
---
+++ user/Sign.java
@@ -0,0 +1,14 @@
+package user;
+
+public class Sign implements Runnable{
+    private Storage st;
+    private char num;
+    Sign(Storage st){
+  	  this.st=st;
+    }
+    public void run(){
+  	  while(true){
+  		  st.put(num++);
+  	  }
+    }
+}
